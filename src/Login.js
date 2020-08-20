3704b1a56734048b169af789696eef62a706d371
Yuchen Peng <yuchenpeng100@gmail.com>
2020-08-19 23:21:42 -0400

    starting

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Login.css';
+import { loginUrl } from './spotify';
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://www.basspromotions.com/wp-content/uploads/2019/05/Spotify_Logo_RGB_White.png"
+                alt="spotify img"
+            />
+            <a href={loginUrl}>Login with Spotify</a>  
+        </div>
+    )
+}
+
+export default Login
